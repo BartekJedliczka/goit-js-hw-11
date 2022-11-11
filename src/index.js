bbaa04757e6ea731a42610acf20b9a99e169fb1d
BartekJedliczka <bartekjedliczka@gmail.com>
2022-11-11 17:27:57 +0100

    task 11

--- src/index.js
+++ src/index.js
@@ -9,3 +9,25 @@ const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 
 btnLoadMore.style.display = 'none'; //zobaczyć
+
+btnSearch.addEventListener('click', e => {
+  e.preventDefault();
+  cleanGallery();
+  const trimmedValue = input.value.trim();
+  if (trimmedValue !== '') {
+    fetchImages(trimmedValue, pageNumber).then(foundData => {
+      if (foundData.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderImageList(foundData.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${foundData.totalHits} images.`
+        );
+        btnLoadMore.style.display = 'block';
+        gallerySimpleLightbox.refresh();
+      }
+    });
+  }
+});
