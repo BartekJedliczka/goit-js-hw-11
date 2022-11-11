ea6e8e35e8d5bffc69965ff12cad7fcc43e2ae60
BartekJedliczka <bartekjedliczka@gmail.com>
2022-11-11 16:21:51 +0100

    task 11

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { fetchImages } from './js/fetchImages';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const input = document.querySelector('.search-form-input');
+const btnSearch = document.querySelector('.search-form-button');
+const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
+
+btnLoadMore.style.display = 'none'; //zobaczyć
